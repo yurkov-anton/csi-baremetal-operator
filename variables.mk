9cf878908ec8abedae852aba1268d9dbb610d3a1
yurkov-anton <36742876+yurkov-anton@users.noreply.github.com>
2021-04-20 17:30:56 +0300

    Create variables.mk

---
+++ variables.mk
@@ -0,0 +1,32 @@
+# project name
+PROJECT          := csi-baremetal-operator
+
+# ### file paths
+# DRIVER_CHART_PATH		:= charts/csi-baremetal-driver
+# OPERATOR_CHART_PATH		:= charts/csi-baremetal-operator
+# SCHEDULER_CHART_PATH	:= charts/csi-baremetal-scheduler
+# EXTENDER_CHART_PATH		:= charts/csi-baremetal-scheduler-extender
+
+### version
+MAJOR            := 0
+MINOR            := 0
+PATCH            := 13
+PRODUCT_VERSION  ?= ${MAJOR}.${MINOR}.${PATCH}
+BUILD_REL_A      := $(shell git rev-list HEAD |wc -l)
+BUILD_REL_B      := $(shell git rev-parse --short HEAD)
+BLD_CNT          := $(shell echo ${BUILD_REL_A})
+BLD_SHA          := $(shell echo ${BUILD_REL_B})
+RELEASE_STR      := ${BLD_CNT}.${BLD_SHA}
+FULL_VERSION     := ${PRODUCT_VERSION}-${RELEASE_STR}
+TAG              := ${FULL_VERSION}
+BRANCH           := $(shell git rev-parse --abbrev-ref HEAD)
+
+### go env vars
+GO_ENV_VARS     := GO111MODULE=on ${GOPRIVATE_PART} ${GOPROXY_PART}
+
+### custom variables that could be ommited
+GOPRIVATE_PART  :=
+GOPROXY_PART    := GOPROXY=https://proxy.golang.org,direct
+
+# override some of variables, optional file
+-include variables.override.mk
